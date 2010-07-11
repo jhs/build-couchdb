0176dd7e58494e83e385136e726ead046ac6bece
Jason Smith <jhs@couch.io>
2010-07-11 16:27:27 +0700

    Properly source the ruby-inabox script

--- env.sh
+++ env.sh
@@ -3,8 +3,19 @@
 # Activate the CouchDB environment. This script is idempotent and runs silently when not connected to a terminal.
 #
 
+this_file="$BASH_SOURCE"
+if [ -z "$this_file" ]; then
+    this_file="$0"
+fi
+
+# Set this for ruby-inabox to find this project.
+project_parent=$(dirname "$this_file")
+ruby_env_sh="$project_parent/dependencies/ruby-inabox/env.sh"
+unset this_file
+
 # TODO: The idea here is, if Rake is available from the system, just skip the Ruby build.
 #       For now, this just wraps ruby-inabox since that will call the Rake task.
-. dependencies/ruby-inabox/env.sh
+. "$ruby_env_sh"
+unset ruby_env_sh
 
 # vim: sts=4 sw=4 et
