99629ba558064c0e076dd9c477e0c14747592fb8
Jason Smith <jhs@couchone.com>
2010-11-04 18:41:12 -0700

    Move places in lib.rb where they belong

--- tasks/lib.rb
+++ tasks/lib.rb
@@ -1,5 +1,6 @@
 # Miscellaneous utilities
 
+require 'uri'
 require 'find'
 require 'tmpdir'
 
@@ -39,7 +40,6 @@ unless ARGV[0] == 'environment:shell'
   end
 end
 
-require File.dirname(__FILE__) + '/places'
 require File.dirname(__FILE__) + '/distros'
 
 def package_dep opts
@@ -276,3 +276,22 @@ module Rake
     end
   end
 end
+
+#
+# Build Places
+#
+
+DEPS = "#{HERE}/dependencies"
+BUILD = ENV['prefix'] || "#{HERE}/build"
+JS_LIB = "#{BUILD}/bin/js-config"
+ERL_BIN = "#{BUILD}/bin/erl"
+ICU_BIN = "#{BUILD}/bin/icu-config"
+COUCH_SOURCE = ENV['git'] ? git_checkout(ENV['git'], :noop => true) : "#{DEPS}/couchdb"
+COUCH_BUILD = ENV['couchdb_build'] || BUILD
+COUCH_BIN = "#{COUCH_BUILD}/bin/couchdb"
+MANIFESTS = "#{BUILD}/manifests"
+
+PIDS = "#{BUILD}/var/run/couchdb"
+
+AUTOCONF_213 = "#{BUILD}/bin/autoconf2.13"
+AUTOCONF_259 = "#{BUILD}/bin/autoconf2.59"
